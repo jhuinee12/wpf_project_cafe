d34453765676d3b2c3aa69bfe59d74ce9d864fd5
jhuinee <jhuinee12@gmail.com>
2021-02-03 18:38:49 +0900

    sdfg

---
+++ PaymentMethod.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_project_Cafe
+{
+    /// <summary>
+    /// PaymentMethod.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class PaymentMethod : UserControl
+    {
+        DBSqlite dbs = new DBSqlite();
+        Variable variable = new Variable();
+        public PaymentMethod(Variable variable)
+        {
+            this.variable = variable;
+            InitializeComponent();
+        }
+        private void btn_Card_Click(object sender, RoutedEventArgs e)
+        {
+            variable.payment_method = "card";
+            LoadStlm();
+            Stml stml = new Stml();
+            this.Content = stml;
+        }
+        private void btn_Pay_Click(object sender, RoutedEventArgs e)
+        {
+            variable.payment_method = "cash";
+            LoadStlm();
+            Stml stml = new Stml();
+            this.Content = stml;
+        }
+
+        public void LoadStlm()
+        {
+            try // 영수증 목록이 있으면
+            {
+                // 영수증 번호 생성 (마지막 영수증 번호 +1)
+                variable.stlm_number = Int32.Parse(dbs.DataLoad("stlm", "order by stlm_number desc limit 1", "stlm_number")) + 1;
+            }
+            catch (Exception ex)     // 영수증 목록이 없으면
+            {
+                MessageBox.Show(ex.ToString());
+                variable.stlm_number = 1;    // 영수증 번호 : 1
+            }
+
+            string datetime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"); // 구매 시간
+
+            MessageBox.Show("<" + variable.stlm_number + "> 영수증" + "\npayment_list : " + variable.payment_list + "\nprice : " + variable.sum_price);
+
+            // 위에서 뽑아낸 값들을 stlm 테이블에 insert
+            string query = "insert into stlm values ("
+                + variable.stlm_number + ",\"\",\"" + variable.payment_list + "\"," + variable.sum_price + ", \""
+                + variable.payment_method + "\",\"" + variable.place + "\",\"" + datetime + "\")";
+            dbs.InsertColumn(query);
+            dbs.StlmLoadData(variable.stlm_number.ToString());   // 현재 영수증 테이블 로드
+        }
+    }
+}
---
+++ Variable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_project_Cafe
+{
+    public class Variable
+    {
+        public int product_price;       // 제품 가격
+        public int stlm_number;         // 영수증 번호
+        public int sum_price;
+
+        public string product_number;  // 제품번호
+        public string payment_list;    // 리스트뷰에 담은 제품 목록
+        public string place;           // 장소 선택
+        public string payment_method;  // 결제방법
+    }
+}
